8b094eaf623f85b10c6106b918d44e4a1012c5a8
Jean P. Cebinel <92955454+JeanPCB@users.noreply.github.com>
2022-04-04 01:53:13 -0300

    script alterado com sucesso

---
+++ index.js
@@ -0,0 +1,34 @@
+// FUNÇAO FOCAR AREA DE TEXTO (PAGINA INICIADA E NO CLIQUE)
+const focusText = function () {
+    document.querySelector('#text-area').focus();
+}
+
+window.onload = focusText;
+
+document.onclick = focusText;
+
+// FUNÇAO CONTAR:
+let areaTexto;
+let numCaracateres;
+let numPalavras;
+let caracteresEsp;
+
+//O innerText do contador recebe o valor do comprimento do texto
+function funcaoContar() {
+    areaTexto = document.querySelector('#text-area').value;
+
+    if (areaTexto === '') {
+        //Evitando Bug Descoberto
+        document.querySelector('#num-caracteres').textContent = 0;
+        document.querySelector('#num-palavras').textContent = 0;
+    } else {
+        numCaracateres = areaTexto.length;
+
+        //Contar e reescrever o numero de caracteres
+        document.querySelector('#num-caracteres').textContent = numCaracateres;
+
+        //Contar e reescrever o numero de palavras
+        numPalavras = areaTexto.match(/(\w+)?[a-zA-Z0-9àáéíóúãõ][ç]?(\w+)?/gi).length;
+        document.querySelector('#num-palavras').textContent = numPalavras;
+    }
+}
